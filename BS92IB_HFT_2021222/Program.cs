5bc6a583d6f087960068635765e4fed6698cd709
simocsongor.z@stud.uni-obuda.hu <simocsongor.z@stud.uni-obuda.hu>
2022-03-20 16:37:37 +0100

    Add project files.

---
+++ BS92IB_HFT_2021222/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BS92IB_HFT_2021222
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
