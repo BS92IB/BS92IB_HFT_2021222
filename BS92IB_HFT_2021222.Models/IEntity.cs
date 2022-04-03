db3fc8348c66a36f5dae41e95fd94325a67b362e
simocsongor.z@stud.uni-obuda.hu <simocsongor.z@stud.uni-obuda.hu>
2022-04-03 20:49:18 +0200

    IEntity interface creared in Models, will be used for later convenience.

---
+++ BS92IB_HFT_2021222.Models/IEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BS92IB_HFT_2021222.Models
+{
+    public interface IEntity
+    {
+        public int Id { get; }
+    }
+}
