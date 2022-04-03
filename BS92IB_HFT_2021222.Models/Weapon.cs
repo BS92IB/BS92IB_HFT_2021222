32acc6a50deaec1ffd050face46c78da98b92ceb
simocsongor.z@stud.uni-obuda.hu <simocsongor.z@stud.uni-obuda.hu>
2022-04-03 20:48:35 +0200

    Weapon model created in Models.

---
+++ BS92IB_HFT_2021222.Models/Weapon.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BS92IB_HFT_2021222.Models
+{
+    public class Weapon : IEntity
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Designation { get; set; }
+        public string WeaponType { get; set; }
+    }
+}
