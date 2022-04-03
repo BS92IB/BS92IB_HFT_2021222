0a314b97c5e13fb5e4013abbd832efb399aacc19
simocsongor.z@stud.uni-obuda.hu <simocsongor.z@stud.uni-obuda.hu>
2022-04-03 20:48:18 +0200

    Armament model created in Models.

---
+++ BS92IB_HFT_2021222.Models/Armament.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace BS92IB_HFT_2021222.Models
+{
+    public class Armament : IEntity
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Quantity { get; set; }
+
+        [ForeignKey(nameof(Ship))]
+        public int ShipId { get; set; }
+
+        [JsonIgnore]
+        public virtual Ship Ship { get; set; }
+
+        [ForeignKey(nameof(Weapon))]
+        public int WeaponId { get; set; }
+        public virtual Weapon Weapon { get; set; }
+    }
+}
