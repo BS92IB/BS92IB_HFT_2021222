f2ee17ff7b4a91d96302946e8c1e6510ceac210d
simocsongor.z@stud.uni-obuda.hu <simocsongor.z@stud.uni-obuda.hu>
2022-04-03 20:47:36 +0200

    Fleet model created in Models

---
+++ BS92IB_HFT_2021222.Models/Fleet.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BS92IB_HFT_2021222.Models
+{
+    public class Fleet : IEntity
+    {
+        public Fleet()
+        {
+            Ships = new HashSet<Ship>();
+        }
+
+        [Key]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public virtual ICollection<Ship> Ships { get; set; }
+    }
+}
