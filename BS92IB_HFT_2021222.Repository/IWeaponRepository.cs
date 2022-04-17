1ca5a3976f326cdf3cb27082451e9d86a09f6aa4
simocsongor.z@stud.uni-obuda.hu <simocsongor.z@stud.uni-obuda.hu>
2022-04-17 20:19:35 +0200

    Interface for WeaponRepository created.

---
+++ BS92IB_HFT_2021222.Repository/IWeaponRepository.cs
@@ -0,0 +1,18 @@
+﻿using BS92IB_HFT_2021222.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BS92IB_HFT_2021222.Repository
+{
+    public interface IWeaponRepository
+    {
+        void Create(Weapon weapon);
+        Weapon Read(int id);
+        void Update(Weapon weapon);
+        void Delete(int id);
+        IQueryable<Weapon> ReadAll();
+    }
+}
