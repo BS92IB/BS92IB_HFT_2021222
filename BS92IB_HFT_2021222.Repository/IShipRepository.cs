92e975ae5917bb818aea9e4acd9fd302fb1fee47
simocsongor.z@stud.uni-obuda.hu <simocsongor.z@stud.uni-obuda.hu>
2022-04-17 20:18:01 +0200

    Interface for ShipRepository created.

---
+++ BS92IB_HFT_2021222.Repository/IShipRepository.cs
@@ -0,0 +1,18 @@
+﻿using BS92IB_HFT_2021222.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BS92IB_HFT_2021222.Repository
+{
+    public interface IShipRepository
+    {
+        void Create(Ship ship);
+        Ship Read(int id);
+        void Update(Ship ship);
+        void Delete(int id);
+        IQueryable<Ship> ReadAll();
+    }
+}
