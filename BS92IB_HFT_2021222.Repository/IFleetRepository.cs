e4e8fca6120f440ebf3426ccb68dd18e785382c4
simocsongor.z@stud.uni-obuda.hu <simocsongor.z@stud.uni-obuda.hu>
2022-04-17 20:17:30 +0200

    Interface for FleetRepository created.

---
+++ BS92IB_HFT_2021222.Repository/IFleetRepository.cs
@@ -0,0 +1,18 @@
+﻿using BS92IB_HFT_2021222.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BS92IB_HFT_2021222.Repository
+{
+    public interface IFleetRepository
+    {
+        void Create(Fleet fleet);
+        Fleet Read(int id);
+        void Update(Fleet fleet);
+        void Delete(int id);
+        IQueryable<Fleet> ReadAll();
+    }
+}
