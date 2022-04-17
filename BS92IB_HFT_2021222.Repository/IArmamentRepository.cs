cb7019784a6acae010251654ad2ef61dd31bb822
simocsongor.z@stud.uni-obuda.hu <simocsongor.z@stud.uni-obuda.hu>
2022-04-17 20:19:00 +0200

    Interface for ArmamentRepository created.

---
+++ BS92IB_HFT_2021222.Repository/IArmamentRepository.cs
@@ -0,0 +1,18 @@
+﻿using BS92IB_HFT_2021222.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BS92IB_HFT_2021222.Repository
+{
+    public interface IArmamentRepository
+    {
+        void Create(Armament armament);
+        Armament Read(int id);
+        void Update(Armament armament);
+        void Delete(int id);
+        IQueryable<Armament> ReadAll();
+    }
+}
