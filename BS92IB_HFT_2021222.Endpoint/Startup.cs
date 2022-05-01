d66cc48662e004c7cb059c23242579d52f2b8003
simocsongor.z@stud.uni-obuda.hu <simocsongor.z@stud.uni-obuda.hu>
2022-05-01 21:36:52 +0200

    Modified Startup

--- BS92IB_HFT_2021222.Endpoint/Startup.cs
+++ BS92IB_HFT_2021222.Endpoint/Startup.cs
@@ -1,3 +1,6 @@
+using BS92IB_HFT_2021222.Data;
+using BS92IB_HFT_2021222.Logic;
+using BS92IB_HFT_2021222.Repository;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -16,6 +19,16 @@ namespace BS92IB_HFT_2021222.Endpoint
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllers();
+            services.AddTransient<IFleetLogic, FleetLogic>();
+            services.AddTransient<IFleetRepository, FleetRepository>();
+            services.AddTransient<IShipLogic, ShipLogic>();
+            services.AddTransient<IShipRepository, ShipRepository>();
+            services.AddTransient<IArmamentLogic, ArmamentLogic>();
+            services.AddTransient<IArmamentRepository, ArmamentRepository>();
+            services.AddTransient<IWeaponLogic, WeaponLogic>();
+            services.AddTransient<IWeaponRepository, WeaponRepository>();
+            services.AddTransient<NavyDbContext, NavyDbContext>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -30,10 +43,7 @@ namespace BS92IB_HFT_2021222.Endpoint
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllers();
             });
         }
     }
