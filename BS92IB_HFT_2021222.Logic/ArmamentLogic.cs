b63d1e8c0a99313d2de334a65d5714d0eefb6615
simocsongor.z@stud.uni-obuda.hu <simocsongor.z@stud.uni-obuda.hu>
2022-05-01 21:34:09 +0200

    Finished IArmamentLogic and ArmamentLogic

---
+++ BS92IB_HFT_2021222.Logic/ArmamentLogic.cs
@@ -0,0 +1,51 @@
+﻿using BS92IB_HFT_2021222.Models;
+using BS92IB_HFT_2021222.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BS92IB_HFT_2021222.Logic
+{
+    public class ArmamentLogic : IArmamentLogic
+    {
+        IArmamentRepository armamentRepo;
+        public ArmamentLogic(IArmamentRepository armamentRepo)
+        {
+            this.armamentRepo = armamentRepo;
+        }
+        public void Create(Armament armament)
+        {
+            if (string.IsNullOrEmpty(armament.Name))
+            {
+                throw new ArgumentException("Name cannot be Null.");
+            }
+            if (armament.Quantity <= 0)
+            {
+                throw new ArgumentException("Quantity cannot be <= 0");
+            }
+            armamentRepo.Create(armament);
+        }
+
+        public void Delete(int id)
+        {
+            armamentRepo.Delete(id);
+        }
+
+        public Armament Read(int id)
+        {
+            return armamentRepo.Read(id);
+        }
+
+        public IEnumerable<Armament> ReadAll()
+        {
+            return armamentRepo.ReadAll();
+        }
+
+        public void Update(Armament armament)
+        {
+            armamentRepo.Update(armament);
+        }
+    }
+}
---
+++ BS92IB_HFT_2021222.Logic/IArmamentLogic.cs
@@ -0,0 +1,18 @@
+﻿using BS92IB_HFT_2021222.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BS92IB_HFT_2021222.Logic
+{
+    public interface IArmamentLogic
+    {
+        void Create(Armament armament);
+        Armament Read(int id);
+        void Update(Armament armament);
+        void Delete(int id);
+        IEnumerable<Armament> ReadAll();
+    }
+}
