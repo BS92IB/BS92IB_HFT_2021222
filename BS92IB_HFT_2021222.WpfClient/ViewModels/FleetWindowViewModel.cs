798f80177b30831905d0880e34b0003078657a18
simocsongor.z@stud.uni-obuda.hu <simocsongor.z@stud.uni-obuda.hu>
2023-12-09 12:26:39 +0100

    WPF client work in progress

---
+++ BS92IB_HFT_2021222.WpfClient/ViewModels/FleetWindowViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BS92IB_HFT_2021222.WpfClient.ViewModels
+{
+    class FleetWindowViewModel
+    {
+    }
+}
---
+++ BS92IB_HFT_2021222.WpfClient/ViewModels/WeaponWindowViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BS92IB_HFT_2021222.WpfClient.ViewModels
+{
+    public class WeaponWindowViewModel:ObservableRecipient
+    {
+    }
+}
